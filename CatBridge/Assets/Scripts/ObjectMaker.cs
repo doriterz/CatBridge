317a109d872194858e4103c4a3a30d7211d76263
dKim <rlaehdud@gmail.com>
2020-09-15 12:39:44 +0900

    TestrunV2
    Drag&Drop fixed.
    
    instantiating on a scene needs to be worked on.

---
+++ CatBridge/Assets/Scripts/ObjectMaker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMaker : MonoBehaviour
+{
+    private PositionController positionController;
+
+    public GameObject[] platformObjects;
+    public GameObject[] movementObjects;
+    public GameObject platformPositions;
+    public GameObject movementObjectsPostitions;
+
+    public int requiredPlatforms = 5;
+    public int requiredMovements = 3;
+    public int howmanyNeeded = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionController = FindObjectOfType<PositionController>();
+
+
+        for (int i = 0; i < requiredPlatforms; i++)
+        {
+            for (int j = 0; j < howmanyNeeded; j++)
+            {
+            GameObject platform = Instantiate(platformObjects[i], new Vector3(platformPositions.transform.position.x + (i*5) + j, platformPositions.transform.position.y, platformPositions.transform.position.z), Quaternion.identity);
+
+            platform.name = "platform" + i + " No." + j;                
+            }
+        }  
+
+        for (int i = 0; i < requiredMovements; i++)
+        {
+            for (int j = 0; j < howmanyNeeded; j++)
+            {
+            GameObject movements = Instantiate(movementObjects[i], new Vector3(movementObjectsPostitions.transform.position.x + (i*5) + j, movementObjectsPostitions.transform.position.y, movementObjectsPostitions.transform.position.z), Quaternion.identity);
+
+            movements.name = "movements" + i + " No." + j;                
+            }
+        }  
+
+
+
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- CatBridge/Assets/Scripts/ObjectMovement.cs
+++ CatBridge/Assets/Scripts/ObjectMovement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isBeingHeld == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
     }
+
+    private void OnMouseDown() 
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+            Debug.Log("HELD");
+
+        }
+
+    }
+
+    private void OnMouseUp() 
+    {
+        
+
+        isBeingHeld = false;
+        Debug.Log("NOT HELD");
+    }
+
+
 }
